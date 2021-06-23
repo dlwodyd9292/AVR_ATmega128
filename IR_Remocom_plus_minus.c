e1d60be8a313454396a1d3a40c5cffe56798b895
dlwodyd9292 <77718386+dlwodyd9292@users.noreply.github.com>
2021-06-23 12:25:15 +0900

    Update IR_Remocom_plus_minus.c

--- IR_Remocom_plus_minus.c
+++ IR_Remocom_plus_minus.c
@@ -6,7 +6,7 @@
 FILE OUTPUT = FDEV_SETUP_STREAM(UART0_transmit, NULL, _FDEV_SETUP_WRITE);
 FILE INPUT = FDEV_SETUP_STREAM(NULL, UART0_receive, _FDEV_SETUP_READ);
 
-volatile int bitCount;
+volatile int bitCount; //32bit(4byte)를 센다
 volatile unsigned long receivedData;
 int LED_count = 4;
 
@@ -43,41 +43,44 @@ void print_received_data(int repeat)
 ISR(INT0_vect)
 {
 	int time = TCNT0;
-	int overflow = TIFR & (1 << TOV0);
+	int overflow = TIFR & (1 << TOV0); //오버플로우 발생했을 때 값을 overflow로 저장 
 	if(bitCount == 32)
 	{
-		if ((time > 201) && (time < 221) && (overflow == 0)) 
+		if ((time > 201) && (time < 221) && (overflow == 0)) // 일반 데이터 리드 코드 13.5ms = 약 211 클록
 		{
 			receivedData = 0;
 			bitCount = 0;
 		}
-		else if((time > 166) && (time < 186) && (overflow == 0))
+		else if((time > 166) && (time < 186) && (overflow == 0)) // 반복 데이터 리드 코드 11.25ms = 약 176 클록
 		{
 			print_received_data(1);
 		}
 		else
 		{
-			bitCount = 32;
+			bitCount = 32; // 잘못된 신호인 경우 재시작 
 		}
 	}
-	else
+	else				// 논리 0 : 약 18클록, 논리 1 : 약 35 클록
 	{
-		if((time > 40) || (overflow != 0))
-		bitCount = 32;
+		if((time > 40) || (overflow != 0)) // 신호 간격이 너무 긴 경우
+		bitCount = 32; // 재시작 
 		else
 		{
-			if(time > 26)
-			receivedData = (receivedData << 1) + 1;
+			if(time > 26) 		// 1을 수신 했을 떄((18+35) / 2)
+				receivedData = (receivedData << 1) + 1;
 			else
-			receivedData = (receivedData << 1);
-			if(bitCount == 31)
-			print_received_data(0);
+				receivedData = (receivedData << 1);
+			
+			if(bitCount == 31)	// 4byte 데이터 수신 완료 
+				print_received_data(0);
+			
 			bitCount++;
 		}
 	}
 	
-	TCNT0 = 0;
-	TIFR = TIFR | (1 << TOV0);
+	TCNT0 = 0; // 0번 타이머/카운터 클리어
+	
+	TIFR = TIFR | (1 << TOV0); // ISR 륀이 호출되지 않으므로 자동으로 클리어 되지 않으므로 클리어 시켜주는 코드를 넣어줘야 한다. 
 }
 
 int main(void)
@@ -87,9 +90,10 @@ int main(void)
 	
 	UART0_init();
 	
-	TCCR0 |= (1 << CS00) | (1 << CS01) | (1 << CS02);
-	EIMSK |= (1 << INT0);
-	EICRA |= (1 << ISC01);
+	TCCR0 |= (1 << CS00) | (1 << CS01) | (1 << CS02);  // 분주비 1024
+	
+	EIMSK |= (1 << INT0);				  // INT0 인터럽트 활성화
+	EICRA |= (1 << ISC01);				  // 하강 에지에서 인터럽트 발생
 	sei();
 	
 	bitCount = 32;
